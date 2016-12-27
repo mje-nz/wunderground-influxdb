678674a5ec376109a43c0f937f39fa23d1ed66c8
Matthew Edwards <matthew@matthewedwards.co.nz>
2016-12-27 13:46:32 +1300

    Poll multiple stations one after another, warn on non-200 status or old data

--- main.py
+++ main.py
@@ -58,6 +58,9 @@ class WundergroundConditions(object):
 
 def fetch_conditions_from_wunderground(api_key, station_name):
     response = requests.get("http://api.wunderground.com/api/%s/conditions/q/%s.json" % (api_key, station_name))
+    if response.status_code != 200:
+        print('WARNING: got status code', response.status_code)
+        print(response.json())
     return WundergroundConditions(station_name, response.json())
 
 
@@ -87,19 +90,24 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-k', '--key', required=True, dest='api_key',
                         help='Weather Underground API key')
-    parser.add_argument('-s', '--station', required=True, dest='station_name',
-                        help='Weather Underground station ID or location search')
     parser.add_argument('-o', '--host', default='localhost', metavar='HOSTNAME',
                         help='InfluxDB hostname', dest='influxdb_hostname')
     parser.add_argument('-b', '--database', default='weather', metavar='DATABASE',
                         help='InfluxDB database', dest='influxdb_database')
+    parser.add_argument('-s', '--station', required=True, action='append', dest='station_names',
+                        help='Weather Underground station ID or location search')
     args = parser.parse_args()
 
-    conditions = fetch_conditions_from_wunderground(args.api_key, args.station_name)
-    print('Got %s' % conditions)
-    client = influxdb.InfluxDBClient(host=args.influxdb_hostname, database=args.influxdb_database)
-    write_conditions_to_influxdb(conditions, client)
-    print('Written to %s on %s' % (args.influxdb_database, args.influxdb_hostname))
+    for station_name in args.station_names:
+        conditions = fetch_conditions_from_wunderground(args.api_key, station_name)
+        print('Got %s' % conditions)
+        age = datetime.datetime.utcnow() - conditions.observation_time
+        if age > datetime.timedelta(minutes=30):
+            rounded_age = datetime.timedelta(seconds=int(age.total_seconds()))
+            print('WARNING: measurement is %s old' % rounded_age)
+        client = influxdb.InfluxDBClient(host=args.influxdb_hostname, database=args.influxdb_database)
+        write_conditions_to_influxdb(conditions, client)
+        print('Written to %s on %s' % (args.influxdb_database, args.influxdb_hostname))
 
 
 if __name__ == '__main__':
